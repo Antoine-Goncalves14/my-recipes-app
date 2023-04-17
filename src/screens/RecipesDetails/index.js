693f560508138e4ad20088d1add4c60ade6f7e3b
Antoine Goncalves <antoine.goncalves-isc.france@ibm.com>
2023-04-17 15:53:49 +0200

    affichage de la recette

--- src/screens/RecipesDetails/index.js
+++ src/screens/RecipesDetails/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, TouchableOpacity} from 'react-native';
 import {useSelector} from 'react-redux';
 import {getSelectedRecipe} from '../../redux/selectors';
+import {useFetchRecipes} from '../../api/recipes/useFetchRecipes';
 
 export default function RecipesDetails({route, navigation}) {
   const {id} = route.params;
   console.log(id);
+  const {getRecipeById} = useFetchRecipes();
 
   const recipe = useSelector(getSelectedRecipe);
+
+  useEffect(() => {
+    getRecipeById(id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
