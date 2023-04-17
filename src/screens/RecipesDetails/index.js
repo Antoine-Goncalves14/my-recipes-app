cb94676b735e5f48552837dade1ec149e704572a
Antoine Goncalves <antoine.goncalves-isc.france@ibm.com>
2023-04-17 16:11:31 +0200

    le style

--- src/screens/RecipesDetails/index.js
+++ src/screens/RecipesDetails/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import {Text, Image, StyleSheet, View, ScrollView} from 'react-native';
 import {useSelector} from 'react-redux';
 import {getSelectedRecipe} from '../../redux/selectors';
 import {useFetchRecipes} from '../../api/recipes/useFetchRecipes';
@@ -16,16 +16,58 @@ export default function RecipesDetails({route, navigation}) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  if (!recipe) {
+    return <View />;
+  }
+
   return (
-    <>
-      <Text>{id}</Text>
-      <Text>{recipe.title}</Text>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.goBack();
-        }}>
-        <Text>Revenir</Text>
-      </TouchableOpacity>
-    </>
+    <ScrollView>
+      <Image source={{uri: recipe.image}} style={styles.image} />
+      <Text style={styles.title}>{recipe.title}</Text>
+      <Text style={styles.caption}>
+        Ready in {recipe.readyInMinutes} minutes
+      </Text>
+      <Text style={styles.caption}>{recipe.summary}</Text>
+      <View style={styles.containerIng}>
+        <Text style={styles.titleIng}>Ingrédients :</Text>
+        {recipe.extentedIngretiens?.map(ing => (
+          <Text>{ing.name}</Text>
+        ))}
+      </View>
+
+      <View style={styles.containerIng}>
+        <Text style={styles.titleIng}>Steps :</Text>
+        {recipe.extentedIngretiens?.map(ing => (
+          <Text>{ing.name}</Text>
+        ))}
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 200,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  caption: {
+    textAlign: 'center',
+  },
+  containerIng: {
+    marginHorizontal: 16,
+    marginVertical: 10,
+    padding: 6,
+    borderTopColor: 'grey',
+    borderTopWidth: 1,
+  },
+  titleIng: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
